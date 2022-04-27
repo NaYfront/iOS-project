af667cf26324138a6940689e03e0e786c90b32dd
NaYfront <nayfront@MacBook-Air-NaYfront.local>
2022-04-27 15:37:09 +0300

    work with xib

--- SleepApp/ListenModule/View/ListenViewController.swift
+++ SleepApp/ListenModule/View/ListenViewController.swift
@@ -14,6 +14,23 @@ class ListenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+        @IBAction func buttonAction(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+
+        switch button.tag {
+        case 0:
+            print("0")
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        case 3:
+            print("3")
+        default:
+            return
+        }
+    }
 }
 
 extension ListenViewController: ListenViewProtocol {
